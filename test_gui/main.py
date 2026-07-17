10fd5650226e6dd54610c41d4283f4e2b29f733d
zanilyx <ritesh4400hi@gmail.com>
2026-07-17 10:57:00 +0530

    cleanup

--- test_gui/main.py
+++
@@ -1,5 +0,0 @@
-import psutil, socket
-for name, addrs in psutil.net_if_addrs().items():
-    for addr in addrs:
-        if addr.family == socket.AF_INET6:
-            print(f"{name}: {addr.address!r}")
